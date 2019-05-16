37504e25d1fedf89a180f28a8eba109c0a39c9a7
yeisonurrea <ing.dev4.tic@ipshorisoes.org>
2019-05-16 11:04:40 -0500

    prueba graficas chart js

--- src/java/com/fut/util/Cons.java
+++ src/java/com/fut/util/Cons.java
@@ -17,4 +17,55 @@ public class Cons {
     public static final String MSG_SUCCESSFUL = "Exitoso";
     public static final String MSG_ERROR = "Error";
     public static final String MSG_WARN = "Aviso";
+    
+    public static final String ARRAY_Y = "{\"label\": \"Z\",\"borderWidth\":1,\"pointRadius\": 0,\"borderColor\": \"rgba(255,0,0,1)\",\"fill\": false,\"data\": [arrayy]}";
+    public static final String ARRAY_GRAP = "{\"labels\": [arraylabel],\"datasets\": [arraydataset]}";
+    
+    
+    
+    
+    public static final String DATOS_JSON = "{\n" +
+"			\"labels\" : [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9],\n" +
+"			\n" +
+"			\"datasets\" : [\n" +
+"			            {\n" +
+"                  \"label\" : \"Z\",\n" +
+"                  \"pointRadius\" : 0,\n" +
+"                  \"borderColor\" : \"rgba(255,0,0,1)\",\n" +
+"                  \"fill\" : false,\n" +
+"                  \"data\" : [{\n" +
+"						\"x\": 0.0,\n" +
+"						\"y\": 3.0\n" +
+"					}, {\n" +
+"						\"x\": 0.5,\n" +
+"						\"y\": 6.0\n" +
+"					}, {\n" +
+"						\"x\": 1.0,\n" +
+"						\"y\": 0.7\n" +
+"					}, {\n" +
+"						\"x\": 1.5,\n" +
+"						\"y\": 6.0\n" +
+"					}]\n" +
+"                  },\n" +
+"			            {\n" +
+"          				\"label\" : \"prueba\",\n" +
+"          				\"borderColor\" : \"rgba(249, 128, 23,1)\",\n" +
+"                  \"pointRadius\" : 2,\n" +
+"                  \"fill\" : false,\n" +
+"          				\"data\" : [{\n" +
+"						\"x\": 0.0,\n" +
+"						\"y\": 2.0\n" +
+"					}, {\n" +
+"						\"x\": 0.5,\n" +
+"						\"y\": 3.3\n" +
+"					}, {\n" +
+"						\"x\": 1.0,\n" +
+"						\"y\": 4.0\n" +
+"					}, {\n" +
+"						\"x\": 1.3,\n" +
+"						\"y\": 4.5\n" +
+"					}]\n" +
+"      			      }\n" +
+"			        ]\n" +
+"		}";
 }
